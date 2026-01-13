193649b6b7d9b162b3ee7dabb21803de47181f49
Avinasha Shetty <163711836+AvinashShetty7@users.noreply.github.com>
2026-01-13 22:50:36 +0530

    Update AdminDashboard.jsx
    updated adminvashbiard

--- frontend/src/components/Admindashboard/AdminDashboard.jsx
+++ frontend/src/components/Admindashboard/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
-import Navbar from "../navbar/navbar";
+import Navbar from "../navbar/Navbar";
 
 export default function AdminDashboard() {
   // const [activeTab, setActiveTab] = useState("register");
